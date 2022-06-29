9e47659b601d03213b77f5764335e7b696730773
SantiagoOspinaO <santiago.m200@outlook.es>
2022-06-28 19:56:18 -0500

    Se actualiza el pom general con la dependencia de sofka

--- domain/src/main/java/org/sofkau/empleado/Empleado.java
+++ domain/src/main/java/org/sofkau/empleado/Empleado.java
@@ -1,4 +1,15 @@
 package org.sofkau.empleado;
 
-public class Empleado {
+import co.com.sofka.domain.generic.AggregateEvent;
+import org.sofkau.empleado.values.EmpleadoId;
+
+public class Empleado extends AggregateEvent<EmpleadoId> {
+
+    protected DatosEmpleado datosEmpleado;
+    protected Curso curso;
+    protected Calificacion calificacion;
+
+    public Empleado(EmpleadoId entityId) {
+        super(entityId);
+    }
 }
